4c10176c92ae467d45146ffb6679fb0dcb14a57f
tekuprogramming <mila.p.06@seznam.cz>
2024-05-29 21:59:14 +0200

    added method testGetConversionRate

--- src/CurrencyConverterTest.java
+++ src/CurrencyConverterTest.java
@@ -1,9 +1,25 @@
 import org.junit.Test;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class CurrencyConverterTest {
 
     @Test
     public void testGetConversionRate() {
-        String base
+        String targetCurrency = "USD";
+
+        CurrencyConverter converter = new CurrencyConverter();
+
+        double expectedRate = 1.0;
+
+        try {
+            double rate = converter.getConversionRate(targetCurrency);
+            assertEquals(expectedRate, rate, 0.001);
+        } catch (IOException e) {
+            fail("IOException should not occur.");
+        }
     }
 }
