4fd1581c980c7d736ad5bb9c2f226639cc7b08a8
tekuprogramming <mila.p.06@seznam.cz>
2024-05-30 17:12:56 +0200

    renamed UserInteractionTester to UserInteractionTest

--- src/UserInteractionTest.java
+++ src/UserInteractionTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class UserInteractionTester {
+public class UserInteractionTest {
 
     @Test
     public void testBuildWeatherUrl_CurrentWeather() {
