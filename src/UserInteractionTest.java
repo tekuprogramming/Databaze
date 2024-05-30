654631d3baa64b785c96334e87ad03e509772ea4
tekuprogramming <mila.p.06@seznam.cz>
2024-05-30 18:32:44 +0200

    added method testStart

--- src/UserInteractionTest.java
+++ src/UserInteractionTest.java
@@ -1,6 +1,11 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class UserInteractionTest {
 
@@ -44,5 +49,27 @@ public class UserInteractionTest {
         String actualUrl = interaction.buildWeatherUrl(cityName, countryName, apiKey, isForecast);
 
         assertEquals("URL should match the expected encoded URL", expectedUrl, actualUrl);
+    }
+
+    @Test
+    public void testStart() {
+        String userInput = "2\n3\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        UserInteraction interaction = new UserInteraction();
+        interaction.start();
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Select a city to get weather data:"));
+        assertTrue(output.contains("Select data type:"));
+        assertTrue(output.contains("Current time in Prague"));
+
+        System.setIn(System.in);
+        System.setOut(System.out);
     }
 }
